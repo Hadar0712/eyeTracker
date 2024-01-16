d921f7fa73c5bf1a784406443b40c3ecc7c67699
Hadar0712 <119673095+Hadar0712@users.noreply.github.com>
2024-01-16 13:16:52 +0200

    Delete deps directory

--- deps/include/cpp_mex_helpers/always_false.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <type_traits>
-
-// helper needed to be able to do static_assert(false,...), e.g. to mark some constexpr if branches as todo
-template <class...> constexpr std::false_type always_false{};
-
